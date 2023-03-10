47590f70a5d22483d15e30bbd0378654ea3854c7
vikiv5 <vi_kov@ukr.net>
2023-03-10 12:11:03 +0200

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -11,6 +11,9 @@ import {
   
   import PropTypes from 'prop-types';
   
+  function getRandomColors(){
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
   export const Statistics = ({ stats, title }) => {
     return (
@@ -20,7 +23,7 @@ import {
 
 <ColorsList>
           {stats.map(stat => (
-            <Color key={stat.id}>
+            <Color key={stat.id}> style={{backgroundColor:getRandomColors()}}
               <Label>{stat.label}</Label>
               <Percentage>{stat.percentage}%</Percentage>
             </Color>
