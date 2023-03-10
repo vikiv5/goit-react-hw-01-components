34abc0daa1e4f188c540fe63c181cb84ff3eae37
vikiv5 <vi_kov@ukr.net>
2023-03-10 02:03:17 +0200

    proptypes

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 import {
     Container,
     UserInfo,
@@ -41,5 +43,17 @@ import {
 
 </Container>);
   }
+
+  Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
+  };
 
   export default Profile ;
